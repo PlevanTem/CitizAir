a470ffb08268018c80f75bf6afb2b5379ed9462b
bufan <178316470@qq.com>
2022-12-11 13:13:12 +0100

    update partile number and motion speed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,11 +8,11 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = -2; i < 3; i++)
+        for (int i = -2; i < 10; i++)
         {
-            for (int j = -2; j < 3; j++)
+            for (int j = -2; j < 5; j++)
             {
-                for (int k = -2; k < 3; k++)
+                for (int k = -2; k < 5; k++)
                 {
                     int rand = Random.Range(0, AtomPrefab.Count);
                     Instantiate(AtomPrefab[rand], new Vector3(i*2f,j * 2f, k * 2f), Quaternion.identity);
